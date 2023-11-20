7893d6b7e6725837def5db29a2b8da9d9ea2f2fe
anyl92 <yulim9592@gmail.com>
2023-11-21 00:41:53 +0900

    feat(expoav): apply expo-av

--- src/features/video/edit/EditVideo.tsx
+++ src/features/video/edit/EditVideo.tsx
@@ -1,15 +1,40 @@
-import { FunctionComponent } from 'react';
-import { Text, View } from 'react-native';
+import { FunctionComponent, useRef, useState } from 'react';
+import { Button, Text, View } from 'react-native';
 import styles from './editVideo.scss';
 import { EditScreenProps } from '../../stackNavigation/types';
+import { Video, ResizeMode, AVPlaybackStatus } from 'expo-av';
 
 const EditVideo: FunctionComponent<EditScreenProps> = ({ route }) => {
-  const videoUri = route.params?.videoUri;
-  console.log(videoUri);
+  const videoUri = route.params?.videoUri || '';
+
+  const video = useRef<Video>(null);
+  const [status, setStatus] = useState<AVPlaybackStatus>(
+    {} as AVPlaybackStatus,
+  );
 
   return (
     <View style={styles.container}>
       <Text>Edit Video</Text>
+      <Video
+        ref={video}
+        style={styles.video}
+        source={{
+          uri: videoUri,
+        }}
+        useNativeControls
+        resizeMode={ResizeMode.CONTAIN}
+        onPlaybackStatusUpdate={setStatus}
+      />
+      {status.isLoaded ? (
+        <Button
+          title={status.isPlaying ? 'Pause' : 'Play'}
+          onPress={() =>
+            status.isPlaying
+              ? video.current?.pauseAsync()
+              : video.current?.playAsync()
+          }
+        />
+      ) : null}
     </View>
   );
 };
