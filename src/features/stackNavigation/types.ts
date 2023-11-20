36305330696022337b3ee9fcdfb07e486aaccc5c
anyl92 <yulim9592@gmail.com>
2023-11-20 18:31:13 +0900

    feat(route): delever navigation route param uri

--- src/features/stackNavigation/types.ts
+++ src/features/stackNavigation/types.ts
@@ -1,7 +1,8 @@
-import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import { NativeStackNavigationProp, NativeStackScreenProps } from '@react-navigation/native-stack';
 
 export type NavigationParamList = {
   Home: undefined;
   Edit: { videoUri: string } | undefined;
 };
 export type NavigationProp = NativeStackNavigationProp<NavigationParamList>;
+export type EditScreenProps = NativeStackScreenProps<NavigationParamList, 'Edit'>;
--- src/features/video/edit/EditVideo.tsx
+++ src/features/video/edit/EditVideo.tsx
@@ -1,8 +1,12 @@
 import { FunctionComponent } from 'react';
 import { Text, View } from 'react-native';
 import styles from './editVideo.scss';
+import { EditScreenProps } from '../../stackNavigation/types';
 
-const EditVideo: FunctionComponent = () => {
+const EditVideo: FunctionComponent<EditScreenProps> = ({ route }) => {
+  const videoUri = route.params?.videoUri;
+  console.log(videoUri);
+
   return (
     <View style={styles.container}>
       <Text>Edit Video</Text>
