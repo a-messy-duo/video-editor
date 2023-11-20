6db9b18f6b49b9f87ed04240af4711da6b83ae02
anyl92 <yulim9592@gmail.com>
2023-11-21 00:46:21 +0900

    chore: change prettierignore

--- src/features/stackNavigation/types.ts
+++ src/features/stackNavigation/types.ts
@@ -1,8 +1,14 @@
-import { NativeStackNavigationProp, NativeStackScreenProps } from '@react-navigation/native-stack';
+import {
+  NativeStackNavigationProp,
+  NativeStackScreenProps,
+} from '@react-navigation/native-stack';
 
 export type NavigationParamList = {
   Home: undefined;
   Edit: { videoUri: string } | undefined;
 };
 export type NavigationProp = NativeStackNavigationProp<NavigationParamList>;
-export type EditScreenProps = NativeStackScreenProps<NavigationParamList, 'Edit'>;
+export type EditScreenProps = NativeStackScreenProps<
+  NavigationParamList,
+  'Edit'
+>;
